9f9b339fd61ec0fc029db56d19fa64fcb0e643ae
jpcanto <jplopessk8@gmail.com>
2019-01-06 15:34:58 -0200

    Adicionando cadastro de relatório

--- dev/src/js/controllers/reportsObjCtrl.js
+++ dev/src/js/controllers/reportsObjCtrl.js
@@ -4,6 +4,10 @@ app.controller("reportsObjCtrl", function($scope, $http){
 
     $http.get('data/daily.json').then(function(response){
         $scope.daily = response.data;
-    })
+    });
+
+    $scope.cadastraRelatorio = function(daily) {
+        $scope.daily.push(angular.copy(daily));
+    };
 
 });
