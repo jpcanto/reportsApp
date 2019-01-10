751b10f45bd6aa0d8579a9e18d9950f48b6fb3df
jpcanto <jplopessk8@gmail.com>
2019-01-10 08:26:28 -0200

    Iniciando construção de light box

---
+++ dev/src/js/mostra-cadastro.js
@@ -0,0 +1,5 @@
+function mostraCadastro() {
+    document.querySelector('header').classList.toggle('desativa');
+    document.querySelector('.main-content').classList.toggle('desativa');
+    document.querySelector('.cadastro').classList.toggle('ativa');
+};
