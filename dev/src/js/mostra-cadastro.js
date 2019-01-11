a37114f6f0585a817e62935b2d4b233a4a08068d
jpcanto <jplopessk8@gmail.com>
2019-01-10 23:39:20 -0200

    Ajustes na visualização do formulario, e criação de botão para fechar formulario

--- dev/src/js/mostra-cadastro.js
+++ dev/src/js/mostra-cadastro.js
@@ -3,3 +3,9 @@ function mostraCadastro() {
     document.querySelector('.main-content').classList.toggle('desativa');
     document.querySelector('.cadastro').classList.toggle('ativa');
 };
+
+function fechaCadastro() {
+    document.querySelector('header').classList.remove('desativa');
+    document.querySelector('.main-content').classList.remove('desativa');
+    document.querySelector('.cadastro').classList.remove('ativa');
+};
