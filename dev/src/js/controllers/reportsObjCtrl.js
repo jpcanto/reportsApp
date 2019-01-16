217b79f0300cd29b9ec4c7e9bef50d65ec0e12d9
jpcanto <jplopessk8@gmail.com>
2019-01-16 20:51:14 -0200

    Iniciando implementação CRUD, firebase

--- dev/src/js/controllers/reportsObjCtrl.js
+++ dev/src/js/controllers/reportsObjCtrl.js
@@ -1,9 +1,26 @@
 
-var app = angular.module("reportsApp", []);
-app.controller("reportsObjCtrl", function($scope){
+var app = angular.module("reportsApp", ['firebase']);
+app.controller("reportsObjCtrl", function($scope, $firebaseArray){
 
-    $scope.cadastraRelatorio = function(daily) {
-        $scope.dailyArray.push(angular.copy(daily));
+    // $scope.cadastraRelatorio = function(daily) {
+    //     $scope.dailyArray.push(angular.copy(daily));
+    // };
+
+    $scope.cadastraRelatorio = function() {
+        var ref = firebase.database().ref("dailyObject");
+        $firebaseArray(ref).$add($scope.daily).then(
+            function(ref){
+                $scope.daily.sprint = "";
+                $scope.daily.apostila = "";
+                $scope.daily.feito = "";
+                $scope.daily.falta = "";
+                $scope.daily.impedimentos = "";
+                $scope.daily.situacao = "";
+            },
+            function(error){
+                console.log(error);
+            }
+        )
     };
 
     getDailyJson();
