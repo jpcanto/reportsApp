00f63798aef58f39bdd9b109c2a1783cbea9e51e
jpcanto <jplopessk8@gmail.com>
2019-03-31 23:03:13 -0300

    Mudança de visual

--- dev/src/js/app.js
+++ dev/src/js/app.js
@@ -1,6 +1,21 @@
-(function(){
+(function () {
     'use strict';
-    
-    angular.module("reportsApp", ['firebase', 'ngAnimate']);
+
+    angular
+        .module("reportsApp", ['firebase', 'ngAnimate', 'ngRoute'])
+        .config(function ($routeProvider) {
+
+            $routeProvider
+                .when('/', {
+                    templateUrl: "views/home.html"
+                })
+
+                .when('/relatorios', {
+                    templateUrl: "views/relatorios.html"
+                })
+
+                .otherwise({ redirectTo: '/' });
+
+        });
 
 })();
