299fba8b5fc312d5a8378442e54078786936e4af
jpcanto <jplopessk8@gmail.com>
2019-04-05 09:01:25 -0300

    Iniciando constru;'ao da p[agina de documenta;'ao

--- dev/src/components/wellcome/wellcome.component.js
+++ dev/src/components/wellcome/wellcome.component.js
@@ -14,8 +14,32 @@
 
         var vm = this;
 
-        vm.activateY = function() {
+        vm.activateY = function () {
             vm.activate = !vm.activate;
+        };
+
+        vm.showDocNav = function() {
+            var elems = document.querySelectorAll('.sidenav');
+            var instances = M.Sidenav.init(elems);
+        };
+
+        vm.cadastraErro = function () {
+            var ref = firebase.database().ref("apoErrors");
+            $firebaseArray(ref).$add(vm.apostila).then(
+                function (ref) {
+                    vm.erro.apostila = "";
+                    vm.erro.pagina = "";
+                    vm.erro.localizacao = "";
+                    vm.erro.descricao = "";
+                    vm.erro.status = "";
+                    vm.erro.academico = "";
+                    vm.erro.situacao = "";
+                    vm.erro.desenvolvedor = "";
+                },
+                function (error) {
+                    console.log(error);
+                }
+            )
         };
 
     };
