04f0ffa120257e8af3dddb8c9ae2d9dd6cb50165
jpcanto <jplopessk8@gmail.com>
2019-04-18 00:08:23 -0300

    Enxugando código index

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -1,3 +1,13 @@
+var config = {
+    apiKey: "AIzaSyBOUSi4PnpCCAL3FnVjBz3E_IZQk6d3wVk",
+    authDomain: "reportswebapp.firebaseapp.com",
+    databaseURL: "https://reportswebapp.firebaseio.com",
+    projectId: "reportswebapp",
+    storageBucket: "reportswebapp.appspot.com",
+    messagingSenderId: "592613138489"
+  };
+  firebase.initializeApp(config);
+
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
