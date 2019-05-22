3ebc7f8d943219f67242a5f4711227aef5d83fe2
jpcanto <jplopessk8@gmail.com>
2019-05-22 19:52:39 -0300

    Ajuste layout botões

--- dev/src/js/controllers/tutorialController.js
+++ dev/src/js/controllers/tutorialController.js
@@ -10,7 +10,22 @@
         var vm = this;
         
         vm.showContent = function () {
-            let getPath = document.getElementById('manual');
+            let getPath = document.getElementById('tutorial-menu');
+            getPath.classList.toggle('active');
+            let getPath2 = document.getElementById('manual');
+            getPath2.classList.toggle('active');
+        };
+
+        vm.showContentScrum = function () {
+            let getPath = document.getElementById('conteudo-scrum');
+            getPath.classList.toggle('active');
+        };
+        vm.showContentDaki = function () {
+            let getPath = document.getElementById('conteudo-daki');
+            getPath.classList.toggle('active');
+        };
+        vm.showContentSquad = function () {
+            let getPath = document.getElementById('conteudo-squad');
             getPath.classList.toggle('active');
         };
 
