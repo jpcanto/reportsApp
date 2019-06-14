23226d7ba2947c8fc3f3f4026051de766e53a6fa
jpcanto <jplopessk8@gmail.com>
2019-06-14 19:22:12 -0300

    Construção página estudos

--- dev/src/js/controllers/estudoController.js
+++ dev/src/js/controllers/estudoController.js
@@ -9,9 +9,18 @@
 
         var vm = this;
 
+        vm.indiceMsg = 'Abrir Índice';
+        vm.arrow = 'arrow_forward';
+
         vm.ativaMenuEstudos = () => {
             vm.activateMenu = !vm.activateMenu;
-            console.log(vm.activateMenu);
+            if(vm.activateMenu == true) {
+                vm.indiceMsg = 'Fechar Índice';
+                vm.arrow = 'arrow_back';
+            } else {
+                vm.indiceMsg = 'Abrir Índice';
+                vm.arrow = 'arrow_forward';
+            }
         };
 
     };
